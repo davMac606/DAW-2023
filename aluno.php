8d21ec07b1b9812d65bff05ebeee4e02c16442d2
davMac606 <cl201238@g.unicamp.br>
2023-02-15 10:51:07 -0300

    exemplo1

---
+++ aluno.php
@@ -0,0 +1,6 @@
+<?php
+echo "First PHP code!";
+echo "Hello World!";
+echo "<br>";
+echo "Simone";
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+//$ra = "121212";
+//$nome = "Simone";
+
+//echo $ra . " " . $nome ;
+
+
+$nome = "Cotil";
+echo $nome;
+echo "<br><br>";
+
+var_dump($nome);
+echo "<br><br>";
+
+$outronome = "unicamp";
+
+echo $nome ."-". $outronome;
+echo "<br><br>";
+
+unset($nome);
+
+
+?>
