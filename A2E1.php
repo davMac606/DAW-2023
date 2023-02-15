e48631fbc5f27533115020dbadfe66c4ea5770cf
davMac606 <cl201238@g.unicamp.br>
2023-02-15 11:40:07 -0300

    altwrações

--- A2E1.php
+++ A2E1.php
@@ -63,7 +63,7 @@ echo "Nome do Aluno:" .Nome_Aluno . "<BR><BR>";
 
 echo "<br><br>";
 
-//echo $_SERVER["SERVER_ADDR"] . "<br>";
+echo $_SERVER["SERVER_ADDR"] . "<br>";
 echo $_SERVER["SERVER_NAME"] . "<br>";
 echo $_SERVER["HTTP_USER_AGENT"] . "<br>";
 echo $_SERVER["REMOTE_ADDR"] . "<br>";
---
+++ a2e.php
@@ -0,0 +1,10 @@
+<?php
+
+//para arredondar valores 
+
+echo round(5.5,0)."<br>"; //6
+
+if (1=="1");
+//=== significa igual em valor e tipo
+
+?>
