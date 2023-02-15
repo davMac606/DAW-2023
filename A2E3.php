00a53d22dd48c1e83e2570ae52f730b121a264d2
davMac606 <cl201238@g.unicamp.br>
2023-02-15 14:40:20 -0300

    switch e afins

--- A2E3.php
+++ A2E3.php
@@ -1,48 +1,33 @@
 <?php
 
-
-//$op = 3;
- //switch($op){
-   // case 1;
+$op = 3;
+ switch($op){
+    case 1;
    // cadastrar();
-    //echo "Cadastro";
-    //break;
+    echo "Cadastro";
+    break;
 
-    //case 2;
+    case 2;
     //emitirRelatorioVenda();
-    //echo "Relatorios";
-    //break;
-    //default:
-    //echo "Opção invalida";
-    //break;
- //}
+    echo "Relatorios";
+    break;
+    default:
+    echo "Opção invalida";
+    break;
+ }
 
- //echo "<br> oi";
-
-
+ echo "<br> oi após break";
 
 
 
+ $x = 2;
+ if($x%2== 0){
+    $resultado = "par";
 
+ }else{
+    $resultado = "ímpar";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
+ }
+ $resultado = ($x%2)==0 ? "par" : "ímpar";
+ echo $resultado;
 ?>
