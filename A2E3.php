ea7df77adf6851ff27fa0cb3b84c34e29e9cfd1a
davMac606 <cl201238@g.unicamp.br>
2023-02-15 14:29:58 -0300

    Exemplo Switch case completo

---
+++ A2E3.php
@@ -0,0 +1,48 @@
+<?php
+
+
+//$op = 3;
+ //switch($op){
+   // case 1;
+   // cadastrar();
+    //echo "Cadastro";
+    //break;
+
+    //case 2;
+    //emitirRelatorioVenda();
+    //echo "Relatorios";
+    //break;
+    //default:
+    //echo "Opção invalida";
+    //break;
+ //}
+
+ //echo "<br> oi";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+?>
