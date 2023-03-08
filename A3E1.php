72a143a437a7a9f8a02796bc62e68c0264b6fd43
davMac606 <cl201238@g.unicamp.br>
2023-03-08 09:56:58 -0300

    ebaa

--- A3E1.php
+++ A3E1.php
@@ -1,31 +1,51 @@
 <?php 
- function calcMedia ($n1, $n2){
-     $media = ($n1 + $n2);
-     return $media;
- 
- }
- 
- 
- //get possibilita a nao criação de valores pre definidos, os coloca na barra do navegador nota1=x&nota2=
- //TROCA GET PRA POST PARA QUE NAO FIQUE VISIVEL
- 
- $n1 =$_GET["nota1"];
- $n2 = $_GET["nota2"];
 
 
- if ( (trim($n1) == "") || (trim($n2) == "")) {
-    echo "<span id='warning'> informe as duas notas!</span>";
- } else{
- $media = calcMedia($n1,$n2)/2;
- echo "Media:" . $media . "<br>";
- 
- if ($media >= 6.0){
-     echo "<span id= 'aprovado'> aprovado! </span>";
- } else{
- 
-     echo "<span id= 'reprovado'> reprovado! </span>";
- }
- 
+$msgN1 = "";
+$msgN2 = "";
+
+
+
+
+
+
+
+if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+
+    function calcMedia ($n1, $n2){
+        $media = ($n1 + $n2);
+        return $media;
+    
+    }
+    
+    
+    //get possibilita a nao criação de valores pre definidos, os coloca na barra do navegador nota1=x&nota2=
+    //TROCA GET PRA POST PARA QUE NAO FIQUE VISIVEL
+    
+    $n1 =$_POST["nota1"];
+    $n2 = $_POST["nota2"];
+   
+   
+    if  (trim($n1) == ""){
+      $msgN1= "A nota 1 é obrigatória";
+    }elseif (trim($n2) == ""){
+    
+        $msgN2= "A nota 2 é obrigatória";
+    }
+    
+    else{
+    $media = calcMedia($n1,$n2)/2;
+    echo "Media:" . $media . "<br>";
+    
+    if ($media >= 6.0){
+        echo "<span id= 'aprovado'> aprovado! </span>";
+    } else{
+    
+        echo "<span id= 'reprovado'> reprovado! </span>";
+    }
+    
+   }
+   
 }
 
 ?>
@@ -48,13 +68,15 @@
     </head>
 <body>
 <h1>Calcula média</h1>
-    <form action="A3E1.php" method="get">
+    <form action="A3E1.php" method="POST">
         Nota 1: <hr>
         
         <input type="text" name="nota1" >
+        <span id="warning"><small><?php echo $msgN1; ?></small></span> 
         <br><br>
         Nota 2 : <hr>
         <input type="text" name="nota2">
+        <span id="warning"><small><?= $msgN2; ?></small></span> 
         <br><br>
     
     
--- modelo.php
+++ modelo.php
@@ -1,31 +1,35 @@
 <?php 
- function calcMedia ($n1, $n2){
-     $media = ($n1 + $n2);
-     return $media;
- 
- }
- 
- 
- //get possibilita a nao criação de valores pre definidos, os coloca na barra do navegador nota1=x&nota2=
- //TROCA GET PRA POST PARA QUE NAO FIQUE VISIVEL
- 
- $n1 =$_GET["nota1"];
- $n2 = $_GET["nota2"];
+if ($_SERVER["REQUEST_METHOD"] === 'POST') {
 
-
- if ( (trim($n1) == "") || (trim($n2) == "")) {
-    echo "<span id='warning'> informe as duas notas!</span>";
- } else{
- $media = calcMedia($n1,$n2)/2;
- echo "Media:" . $media . "<br>";
- 
- if ($media >= 6.0){
-     echo "<span id= 'aprovado'> aprovado! </span>";
- } else{
- 
-     echo "<span id= 'reprovado'> reprovado! </span>";
- }
- 
+    function calcMedia ($n1, $n2){
+        $media = ($n1 + $n2);
+        return $media;
+    
+    }
+    
+    
+    //get possibilita a nao criação de valores pre definidos, os coloca na barra do navegador nota1=x&nota2=
+    //TROCA GET PRA POST PARA QUE NAO FIQUE VISIVEL
+    
+    $n1 =$_GET["nota1"];
+    $n2 = $_GET["nota2"];
+   
+   
+    if ( (trim($n1) == "") || (trim($n2) == "")) {
+       echo "<span id='warning'> informe as duas notas!</span>";
+    } else{
+    $media = calcMedia($n1,$n2)/2;
+    echo "Media:" . $media . "<br>";
+    
+    if ($media >= 6.0){
+        echo "<span id= 'aprovado'> aprovado! </span>";
+    } else{
+    
+        echo "<span id= 'reprovado'> reprovado! </span>";
+    }
+    
+   }
+   
 }
 
 ?>
